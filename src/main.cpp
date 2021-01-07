b189fe1c6cf62a3fe202ad8427975220a2fce424
wass <wassim.filali@gmail.com>
2021-01-07 11:41:21 +0100

    added battery publish and timer sleep

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <WiFi.h>
 
 #include "camera_pins.h"
+#include "battery.h"
+#include "bmm8563.h"
 
 #include <ArduinoJson.h>
 #include <WiFi.h>
@@ -30,10 +32,31 @@ void mqtt_publish_config(){
   String str_config;
   serializeJson(config,str_config);
   String str_topic = config["camera"]["base_topic"];
-  mqtt.publish(str_topic+"/config",str_config,true,2);//LWMQTT_QOS2 = 2
-  mqtt.loop();
+  str_topic += "/config";
+  mqtt.publish(str_topic,str_config,true,2);//LWMQTT_QOS2 = 2
+  Serial.printf("published (%s)=>(%s)\r\n",str_topic.c_str(),str_config.c_str());
 }
 
+
+void mqtt_publish_time(){
+  String str_topic = config["camera"]["base_topic"];
+  str_topic += "/wakeup";
+  float time_f = millis();
+  time_f /=1000;
+  String str_wakeup = String(time_f);
+  mqtt.publish(str_topic,str_wakeup,true,2);//LWMQTT_QOS2 = 2
+  Serial.printf("published (%s)=>(%s)\r\n",str_topic.c_str(),str_wakeup.c_str());
+}
+
+void mqtt_publish_battery(){
+  String str_topic = config["camera"]["base_topic"];
+  str_topic += "/battery";
+  float battery_f = bat_get_voltage();
+  battery_f /=1000;
+  String str_battery = String(battery_f);
+  mqtt.publish(str_topic,str_battery,true,2);//LWMQTT_QOS2 = 2
+  Serial.printf("published (%s)=>(%s)\r\n",str_topic.c_str(),str_battery.c_str());
+}
 
 void camera_start(DynamicJsonDocument &config){
   camera_config_t cam_config;
@@ -79,10 +102,10 @@ void camera_start(DynamicJsonDocument &config){
 
 }
 
-void camera_publish(){
+void mqtt_publish_camera(){
   camera_fb_t * frame = NULL;
 
-  Serial.println("esp_camera_fb_get()");
+  timelog("esp_camera_fb_get()");
   frame = esp_camera_fb_get();
   if (!frame) {
       Serial.println("Camera capture failed");
@@ -95,7 +118,7 @@ void camera_publish(){
   uint8_t * jpg_out;
   size_t jpg_out_len;
 
-  Serial.println("fmt2jpg()");
+  timelog("fmt2jpg()");
   bool res = fmt2jpg(frame->buf, frame_len, width, height, frame->format, quality, &jpg_out, &jpg_out_len);
   if (!res) {
     Serial.println("camera> CAPTURE FAIL");
@@ -104,10 +127,10 @@ void camera_publish(){
 
   Serial.printf("camera> CAPTURE OK %dx%d %db\n", frame->width, frame->height, frame_len);
   String str_topic = config["camera"]["base_topic"];
-  Serial.printf("mqtt> publishing on %s\n",str_topic.c_str());
-  str_topic += "/jpg";//cannot be added in the function call as String overload is missing
-  Serial.printf("publish on(%s) => len(%u)\r\n",str_topic.c_str(),jpg_out_len);
-  mqtt.publish( str_topic.c_str(),reinterpret_cast<const char *>(jpg_out),jpg_out_len,true,2);//LWMQTT_QOS2 = 2
+  str_topic += "/jpg_len";//cannot be added in the function call as String overload is missing
+  mqtt.publish( str_topic,String(jpg_out_len),true,2);//LWMQTT_QOS2 = 2
+  //mqtt.publish( str_topic.c_str(),reinterpret_cast<const char *>(jpg_out),jpg_out_len,true,2);//LWMQTT_QOS2 = 2
+  Serial.printf("published (%s) => len(%u)\r\n",str_topic.c_str(),jpg_out_len);
 }
 
 void setup() {
@@ -116,7 +139,12 @@ void setup() {
   Serial.setDebugOutput(true);
   Serial.println();
   timelog("Boot ready");
-  
+  WiFi.begin(ssid, password);
+
+  bat_init();
+  bmm8563_init();
+  bmm8563_setTimerIRQ(30);//in sec
+
   load_config(config,true);
   timelog("config loaded");
 
@@ -125,33 +153,55 @@ void setup() {
 
   timelog("wifi setup");
 
-  Serial.print("connecting wifi...");
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  Serial.print("checking wifi");
+  int max_timeout = 10;
+  while ((WiFi.status() != WL_CONNECTED)&&(max_timeout>0)) {
     Serial.print(".");
-    delay(1000);
+    delay(500);
+    max_timeout--;
   }
-
+  Serial.print("\n");
+  if(max_timeout == 0){
+    timelog("wifi timeout!");
+    return;
+  }
+  max_timeout = 10;
+  timelog("wifi connected!");
   mqtt.begin(config["mqtt"]["host"],config["mqtt"]["port"], wifi);
-  Serial.print("\nconnecting mqtt...");
-  while (!mqtt.connect(config["mqtt"]["client_id"])) {
+  Serial.print("connecting mqtt");
+  while ((!mqtt.connect(config["mqtt"]["client_id"]))&&(max_timeout>0)) {
     Serial.print(".");
-    delay(1000);
+    delay(500);
+    max_timeout--;
   }
-  Serial.println("\nconnected!");
-
+  if(max_timeout == 0){
+    timelog("MQTT timeout!\n");
+    return;
+  }
+  Serial.print("\n");
+  timelog("mqtt connected!\n");
 
   if(true){
     mqtt_publish_config();
-    mqtt.loop();
+    timelog("=>config");
+    mqtt_publish_battery();
+    timelog("=>battery");
   }
 
-  camera_publish();
+  mqtt_publish_camera();
+  timelog("=>camera");
+  mqtt_publish_time();
+  timelog("=>time");
   mqtt.loop();
-  timelog("setup done");
+  timelog("=>loop");
+
+  timelog("setup done - disabling battery");
+  Serial.flush();
+  bat_disable_output();
+  delay(100);//dies here
 }
 
 void loop() {
+  Serial.println("Cycling with usb power");
   delay(30000);
-  Serial.println("Nothing to do");
 }
